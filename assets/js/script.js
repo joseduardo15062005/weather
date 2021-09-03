ab0acc421dda6402b602d8a42e99140d0314895c
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-09-02 17:12:12 -0700

    add units change to select

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 const APIkey = "d77dd3a350b3fe5cdc671210cf41ca6b";
-const unitsImperial = "imperial";
-const unitsmMtric = "metric";
+
+let units = "imperial";
+const selectUnits = document.getElementById("selectUnits");
+const btnMyLocation = document.getElementById("btnMyLocation");
 
 getCityCurrentWeather("Las Vegas");
 
 function getCityCurrentWeather(city) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unitsImperial}&appid=${APIkey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${APIkey}`;
   fetch(apiUrl)
     .then((response) => response.json())
     .then((response) => {
@@ -16,8 +18,21 @@ function getCityCurrentWeather(city) {
 }
 
 function getCity5DaysForecats(lat, lon) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${unitsImperial}&appid=${APIkey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&appid=${APIkey}`;
   fetch(apiUrl)
     .then((response) => response.json())
     .then((response) => console.log("Forecast 5 Days", response));
 }
+
+function getMyLocationWeather(position) {
+  console.log(position);
+}
+
+selectUnits.addEventListener("change", () => {
+  units = selectUnits.value;
+});
+
+btnMyLocation.addEventListener("click", (event) => {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getMyLocationWeather);
+});
