ac3f6676747db0a340e3f085194b6061021164ba
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-09-02 18:28:30 -0700

    add Basic information to a Weather Card

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,14 @@ const APIkey = "d77dd3a350b3fe5cdc671210cf41ca6b";
 let units = "imperial";
 const selectUnits = document.getElementById("selectUnits");
 const btnMyLocation = document.getElementById("btnMyLocation");
+const btnSearch = document.getElementById("btnSearch");
 
 function getCityCurrentWeather(city) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${APIkey}`;
   fetch(apiUrl)
     .then((response) => response.json())
     .then((response) => {
-      console.log(response);
+      showLocation(response);
       const { lat, lon } = response.coord;
       getCity5DaysForecats(lat, lon);
     });
@@ -19,7 +20,7 @@ function getLocationCurrentWeather(lat, lon) {
   fetch(apiUrl)
     .then((response) => response.json())
     .then((response) => {
-      console.log(response);
+      showLocation(response);
       const { lat, lon } = response.coord;
       getCity5DaysForecats(lat, lon);
     });
@@ -29,7 +30,9 @@ function getCity5DaysForecats(lat, lon) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&appid=${APIkey}`;
   fetch(apiUrl)
     .then((response) => response.json())
-    .then((response) => console.log("Forecast 5 Days", response));
+    .then((response) => {
+      showWeatherInformation(response);
+    });
 }
 
 function getMyLocationWeather(position) {
@@ -44,6 +47,30 @@ function errorLocation(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
+function showLocation(info) {
+  const cityName = document.getElementById("cityName");
+  const today = document.getElementById("today");
+  const weatherIcon = document.getElementById("weatherIcon");
+
+  cityName.textContent = `${info.name} - 03/03/2021 `;
+  const iconUrl = `http://openweathermap.org/img/w/${info.weather[0].icon}.png`;
+  weatherIcon.setAttribute("src", iconUrl);
+}
+
+function showWeatherInformation(info) {
+  console.log(info);
+  const temperature = document.getElementById("temperature");
+  const wind = document.getElementById("wind");
+  const humidity = document.getElementById("humidity");
+  const uvIndex = document.getElementById("uvIndex");
+  const unit = units == "imperial" ? "F" : "C";
+
+  temperature.textContent = `Temperature: ${info.current.temp} ${unit}`;
+  wind.textContent = `Wind: ${info.current.wind_speed} MPH`;
+  humidity.textContent = `Humidity: ${info.current.humidity}%`;
+  uvIndex.textContent = `UV Index: ${info.current.uvi} `;
+}
+
 selectUnits.addEventListener("change", () => {
   units = selectUnits.value;
 });
@@ -51,4 +78,10 @@ selectUnits.addEventListener("change", () => {
 btnMyLocation.addEventListener("click", (event) => {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(getMyLocationWeather, errorLocation);
+});
+
+btnSearch.addEventListener("click", (event) => {
+  const inputCity = document.getElementById("inputCity");
+  event.preventDefault();
+  getCityCurrentWeather(inputCity.value);
 });
